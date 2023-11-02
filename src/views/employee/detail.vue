8505aa4968ce0c6a1ad877d3b133ce1c46d6edb5
xtd0909 <1364094586@qq.com>
2023-11-02 16:08:53 +0800

    SID4001：完成员工详情表单双向数据绑定

--- src/views/employee/detail.vue
+++ src/views/employee/detail.vue
@@ -1,86 +1,99 @@
 <template>
-    <div class="dashboard-container">
-        <div class="app-container">
-            <div class="edit-form">
-                <el-form ref="userForm" label-width="220px">
-                    <!-- 姓名 部门 -->
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="姓名" prop="username">
-                                <el-input size="mini" class="inputW" />
-                            </el-form-item>
-                        </el-col>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <div class="edit-form">
+        <el-form ref="userForm" label-width="220px">
+          <!-- 姓名 部门 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="姓名" prop="username">
+                <el-input v-model="userinfo.username" size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
 
-                    </el-row>
-                    <!-- 工号 入职时间 -->
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="工号" prop="workNumber">
-                                <el-input size="mini" class="inputW" />
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <!--手机 聘用形式  -->
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="手机" prop="mobile">
-                                <el-input size="mini" class="inputW" />
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="部门" prop="departmentId">
-                                <!-- 放置及联部门组件 -->
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="聘用形式" prop="formOfEmployment">
-                                <el-select size="mini" class="inputW" />
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="入职时间" prop="timeOfEntry">
-                                <el-date-picker size="mini" type="date" value-format="yyyy-MM-dd" class="inputW" />
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="转正时间">
-                                <el-date-picker size="mini" type="date" class="inputW" />
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <!-- 员工照片 -->
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item label="员工头像">
-                                <!-- 放置上传图片 -->
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <!-- 保存个人信息 -->
-                    <el-row type="flex">
-                        <el-col :span="12" style="margin-left:220px">
-                            <el-button size="mini" type="primary">保存更新</el-button>
-                        </el-col>
-                    </el-row>
-                </el-form>
-            </div>
+          </el-row>
+          <!-- 工号 入职时间 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="工号" prop="workNumber">
+                <el-input size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!--手机 聘用形式  -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="手机" prop="mobile">
+                <el-input v-model="userinfo.mobile" size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="部门" prop="departmentId">
+                <!-- 放置及联部门组件 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="聘用形式" prop="formOfEmployment">
+                <el-select size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="入职时间" prop="timeOfEntry">
+                <el-date-picker size="mini" type="date" value-format="yyyy-MM-dd" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="转正时间" prop="timeOfEntry">
+                <el-date-picker size="mini" type="date" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 员工照片 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="员工头像" prop="correctionTime">
+                <!-- 放置上传图片 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 保存个人信息 -->
+          <el-row type="flex">
+            <el-col :span="12" style="margin-left:220px">
+              <el-button size="mini" type="primary">保存更新</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
 
-        </div>
     </div>
+  </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      userInfo: {
+        username: '', // 姓名
+        workNumber: '', // 工号
+        mobile: '', // 手机号
+        formOfEmployment: null, // 聘用形式
+        // departmentId: null, // 部门id
+        timeOfEntry: '', // 入职时间
+        correctionTime: '' // 转正时间
+      }
 
+    }
+  }
 }
 </script>
 
