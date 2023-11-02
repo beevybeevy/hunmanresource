94e0121bdfd51bf412237959e65e4944c0c29827
baby <2113341466@qq.com>
2023-11-02 17:00:35 +0800

    权限删除完成

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -21,16 +21,30 @@
         />
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button type="text" size="small">添加</el-button>
+            <el-button type="text" size="small" @click="del(scope.row.id)">添加</el-button>
             <el-button type="text" size="small">编辑</el-button>
             <el-button
+              ref="delPermissions"
               type="text"
               size="small"
-              @click="del(scope.row.id)"
+              @click="handleDelete(scope.row.id)"
             >删除</el-button>
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- 弹出框 -->
+      <el-dialog
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose"
+      >
+        <span>确定要删除该数据吗</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="del()">确 定</el-button>
+        </span>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -41,7 +55,9 @@ export default {
   name: 'Permission',
   data() {
     return {
-      tableData: []
+      tableData: [],
+      dialogVisible: false,
+      id: null
     }
   },
   async created() {
@@ -49,9 +65,20 @@ export default {
     this.tableData = res
   },
   methods: {
-    del(id) {
-      deletePermissions(id)
-      getPermissions()
+    // 弹出框的删除
+    async del() {
+      deletePermissions(this.id)
+      this.dialogVisible = false
+      this.tableData = await getPermissions()
+    },
+    // X 号的点击关闭
+    handleClose() {
+      this.dialogVisible = false
+    },
+    // 打开弹出框
+    handleDelete(id) {
+      this.dialogVisible = true
+      this.id = id
     }
   }
 }
